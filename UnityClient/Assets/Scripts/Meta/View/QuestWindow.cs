692400326ac7395f35fc6584a0e02dbb043ac5b7
KonH <konh@yandex.ru>
2022-06-14 17:33:55 +0200

    Feature: hide QuestWindow while player is in transition

--- UnityClient/Assets/Scripts/Meta/View/QuestWindow.cs
+++ UnityClient/Assets/Scripts/Meta/View/QuestWindow.cs
@@ -1,13 +1,31 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnknownSpace.Meta.Data;
+using VContainer;
 
 namespace UnknownSpace.Meta.View {
 	public sealed class QuestWindow : MonoBehaviour {
+		[SerializeField] GameObject _root;
 		[SerializeField] Button _startButton;
+
+		PlayerData _playerData;
+
+		[Inject]
+		public void Init(PlayerData playerData) {
+			_playerData = playerData;
+		}
 
 		void Awake() {
 			_startButton.onClick.AddListener(OnStartButtonClick);
+		}
+
+		void Update() {
+			var isInTransition = _playerData.TransitionCountdown > 0;
+			var shouldBeActive = !isInTransition;
+			if ( shouldBeActive != _root.gameObject.activeSelf ) {
+				_root.gameObject.SetActive(shouldBeActive);
+			}
 		}
 
 		void OnStartButtonClick() {
